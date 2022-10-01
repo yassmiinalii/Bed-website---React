d1cf9b4a3cd1262106345f4b638ba649e22a008a
yassmiinalii <yasmiiinaliii@gmail.com>
2022-10-02 01:52:55 +0200

    add home to routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,29 @@
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 // import store from "../redux/store";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from '../components/NavBar';
 import './App.scss';
 
+const queryClient = new QueryClient()
 function App() {
-  return (
-       <>
-        <BrowserRouter>
-        <NavBar/>
-          <Routes>
-            {/* <Route path="/" element={} />
-            {/* <Route path="*" element={<NotFound />} /> */}
-          </Routes>
-        </BrowserRouter>
-        </>
+  const Home = lazy(() => import("../pages/Home"));
 
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<div>loooading</div>}>
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </QueryClientProvider>
+    </>
   )
 }
 
