2eb693cb9519302eb291be65bfed7db5a73596e2
yassmiinalii <yasmiiinaliii@gmail.com>
2022-10-02 21:12:35 +0200

    init product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
+import { NumericFormat } from 'react-number-format';
 const ProductCard = (props) => {
   const type = props;
   return (
@@ -14,11 +14,35 @@ const ProductCard = (props) => {
         {/* { type==='MattressesLines'? <Card.Title >Card Title</Card.Title> : ''}
            */}
         <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
+        <div className="card-body-bottom">
+          <div className="card-sub-title d-flex justify-content-between">
+            <p className="product-name">product name</p>
+            <div className="price-container d-flex gap-2">
+            <p className="old-price"> 
+             <NumericFormat
+              value={123}
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"EGP "}/>
+            </p>
+            <p className="current-price">  
+             <NumericFormat
+              value={123}
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"EGP "}/>
+            </p>
+            </div>    
+          </div>
+        <Card.Text> 
+          - Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <div className="btn-action-container d-flex justify-content-evenly">
+        <Button className="cart-btn" style={{color: 'white'}} variant="primary">Add To Cart</Button>
+        <Button className="details-btn" variant="outline-primary">More Details</Button>
+        </div>
+        </div>
       </Card.Body>
     </Card>
   );
