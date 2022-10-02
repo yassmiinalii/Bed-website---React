c22b24a4cddd49ea8316fcc66c7748d9b1b9b599
yassmiinalii <yasmiiinaliii@gmail.com>
2022-10-02 23:34:54 +0200

    fetch all products types data

--- src/components/Mattresses.jsx
+++ src/components/Mattresses.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import ProductCard from './ProductCard';
+import { useSelector, useDispatch } from 'react-redux'
 
+import { useGetAllProductTypesQuery } from '../service/productsTypes'
 const Mattresses = () => {
+    const { data, error, isLoading } = useGetAllProductTypesQuery()
+    const productTypes = data?.message?.productTypes;
+    console.log(productTypes);
+
     return (
-        <Container  className='component-container mattresses-container'>
+        <Container className='component-container mattresses-container'>
             <div className="section-title-container">
-             <h1> Our Mattresses Lines</h1>
-            </div>  
-            <Row>
-                <Col xs={12} md={4} className="productTypes-container d-flex justify-content-center align-items-center"> 
-                <h2 className='productType-title'>
-                Bliss line
-                </h2>
+                <h1> Our Mattresses Lines</h1>
+            </div>
+            {error ? ( <>Oh no, there was an error</>) :
+             isLoading ? ( <>Loading...</>) :
+             data ? ( <>data </>) : null}
+            {/* <Row>   
+                <Col xs={12} md={4} className="productTypes-container d-flex justify-content-center align-items-center">
+                    <h2 className='productType-title'>
+                        Bliss line
+                    </h2>
                 </Col>
-            </Row>   
+            </Row>
             <Row>
-                <Col  xs={12} md={4} >
-                <ProductCard/>
+                <Col xs={12} md={4} >
+                    <ProductCard />
                 </Col>
-            </Row>    
+            </Row> */}
         </Container>
     );
 };
