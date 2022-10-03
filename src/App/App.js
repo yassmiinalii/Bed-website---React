c4c820431236174163e9cab37da2dd695625a709
yassmiinalii <yasmiiinaliii@gmail.com>
2022-10-03 08:05:51 +0200

    add backdrob and nodFound page

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,42 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import { store } from '../redux/store'
-import { Provider } from 'react-redux'
+import * as React from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { store } from "../redux/store";
+import { Provider } from "react-redux";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from '../components/NavBar';
+import NavBar from "../components/navigation/NavBar";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
-import './App.scss';
+import "./App.scss";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 function App() {
   const Home = lazy(() => import("../pages/Home"));
+  const NotFound = lazy(() => import("../pages/NotFound"));
+
 
   return (
     <>
       <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<div>loooading</div>}>
-          <BrowserRouter>
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="*" />
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
-      </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={
+            <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1,}} open={open} >
+            <CircularProgress color="inherit" />
+            </Backdrop>}
+          >
+            <BrowserRouter>
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
+        </QueryClientProvider>
       </Provider>
     </>
-  )
+  );
 }
 
 export default App;
