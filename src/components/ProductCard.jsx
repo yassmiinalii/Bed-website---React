52e7e3180e52740a3e6757cfc8ca3de3605ae744
yassmiinalii <yasmiiinaliii@gmail.com>
2022-10-03 04:24:05 +0200

    replace static data with product data

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,34 @@
 import React from "react";
+import { API_URL, IMAGES_URL } from "../api/BaseUrls";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { NumericFormat } from 'react-number-format';
+import imagePlaceHolder from '../assets/images/imagePlaceHolder.jpeg';
 const ProductCard = (props) => {
-  const type = props;
+  const {type,name,title_english,description_english,beforePrice,afterPrice,product_photos} = props;
+  // console.log(product_photos[0]?.photo_path);
   return (
     <Card style={{ width: "100%" }} className="card-container">
       <Card.Img
         variant="top"
-        src="https://demos-iconcreations.com/schlafmiestrback/assets/productImages/Bliss-Fortune120.jpg"
+        src={product_photos[0]?.photo_path? IMAGES_URL+product_photos[0].photo_path: imagePlaceHolder }
       />
       <Card.Body>
-        {/* { type==='MattressesLines'? <Card.Title >Card Title</Card.Title> : ''}
-           */}
-        <Card.Title>Card Title</Card.Title>
+        {type==='MattressesLines'? <Card.Title >{name}</Card.Title> : ''}
         <div className="card-body-bottom">
           <div className="card-sub-title d-flex justify-content-between">
-            <p className="product-name">product name</p>
+            <p className="product-name">{title_english}</p>
             <div className="price-container d-flex gap-2">
             <p className="old-price"> 
              <NumericFormat
-              value={123}
+              value={beforePrice}
               displayType={"text"}
               thousandSeparator={true}
               prefix={"EGP "}/>
             </p>
             <p className="current-price">  
              <NumericFormat
-              value={123}
+              value={afterPrice}
               displayType={"text"}
               thousandSeparator={true}
               prefix={"EGP "}/>
@@ -35,8 +36,7 @@ const ProductCard = (props) => {
             </div>    
           </div>
         <Card.Text> 
-          - Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          - {description_english}
         </Card.Text>
         <div className="btn-action-container d-flex justify-content-evenly">
         <Button className="cart-btn" style={{color: 'white'}} variant="primary">Add To Cart</Button>
